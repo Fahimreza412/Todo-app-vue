89ecd39ddd33f863c2ef7c0e338a4212ed950de9
Fahimreza412 <fahimreza2210622123@gmail.com>
2025-03-20 01:24:34 +0600

    1.1

---
+++ to-do-app/src/App.vue
@@ -0,0 +1,42 @@
+ <script setup>
+import { ref,onMounted,computed,watch } from "vue";
+const todos = ref([])
+const name = ref('')
+
+const inputContent = ref('')
+const inputCategory = ref(null)
+
+const todoAsync = computed(() => todo.value.sort((a,b) => {
+  return a.createdAt - b.createdAt
+}))
+watch(name,(newVal) => {
+  localStorage.setItem('name',newVal)
+})
+onMounted(()=>{
+  name.value = localStorage.getItem('name') || ''
+})
+const addTodo = () => {}
+</script>
+<template>
+  <!-- <h1 style="text-align: center; margin-top: 10vh;">To-Do-List</h1> -->
+<main class="app">
+<section class="greeting">
+  <h2 class="title">
+    What's up, <input type="text" placeholder="Name here" v-model="name">
+  </h2>
+</section>
+  <section class="create-todo">
+<h3>Create A Todo</h3>
+<form @submit.prevent="addTodo">
+  <h4>What's on your todo List?</h4>
+  <input 
+  type="text"
+   placeholder="e.g make a video"
+    v-model="inputContent" >
+{{ inputContent }}
+</form>
+  </section>
+</main>
+</template> 
+
+
