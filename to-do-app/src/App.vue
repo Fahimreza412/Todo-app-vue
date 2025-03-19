f5fae2445fdc07f293e2e5ad4bc27a839151d6e4
Fahimreza412 <fahimreza2210622123@gmail.com>
2025-03-20 02:02:39 +0600

    1.2

--- to-do-app/src/App.vue
+++ to-do-app/src/App.vue
@@ -27,15 +27,40 @@ const addTodo = () => {}
 </section>
   <section class="create-todo">
 <h3>Create A Todo</h3>
+
 <form @submit.prevent="addTodo">
   <h4>What's on your todo List?</h4>
   <input 
   type="text"
    placeholder="e.g make a video"
     v-model="inputContent" >
-{{ inputContent }}
+<!-- {{ inputContent }} -->
+  <h4>Pick a category</h4>
+<div class="options">
+<label>
+<input type="radio" 
+name="category"
+value="business"
+v-model="inputCategory">
+<span class="bubble business"></span>
+<h2>Business</h2>
+</label>
+
+<label>
+<input type="radio" 
+name="category"
+value="personal"
+v-model="inputCategory">
+<span class="bubble personal"></span>
+<h2>Personal</h2>
+</label>
+<!-- {{ inputCategory }} -->
+</div>
 </form>
   </section>
+
+
+
 </main>
 </template> 
 
